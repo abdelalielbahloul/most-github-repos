b1816dae3a15a339b17a3ad3d2ac6beee61170c8
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-04-19 19:08:07 +0000

    feat : add details models class

---
+++ src/app/models/details-repo.ts
@@ -0,0 +1,13 @@
+export class DetailsRepo {
+    id: number;
+    avatar: string;
+    name: string;
+    description: string;
+    nb_watchers: number;
+    open_issues: number;
+    owner_name: string;
+    submited_time: Date;
+    default_branch: string;
+    license: string;
+
+}
